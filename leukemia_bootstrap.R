482ba74ba50457d3c1520e381ba21df39bec53e6
naiiife <61304007+naiiife@users.noreply.github.com>
2024-12-01 19:52:10 -0500

    Add files via upload

---
+++ leukemia_bootstrap.R
@@ -0,0 +1,337 @@
+set.seed(2024)
+cif1 = cif0 = cif1o = cif0o = cif_spe = cif_cin = cif_int = NULL
+
+for (bs in 1:B){
+cat('Bootstrap round',bs,'of',B,'\n')
+ss1 = sample(which(A==1),replace=TRUE)
+ss0 = sample(which(A==0),replace=TRUE)
+ss = c(ss1,ss0)
+A = dat$A[ss]
+X = as.matrix(dat$X[ss,])
+Tg=dat$Tg[ss];Dg=dat$Dg[ss];Tr=dat$Tr[ss]
+Dr=dat$Dr[ss];Td=dat$Td[ss];Dd=dat$Dd[ss]
+
+# hazard of d
+fit_d = try(phfit_d(Tg,Dg,Tr,Dr,Td,Dd,A,X,a=1))
+if ('try-error' %in% fit_d) next
+Xb = as.numeric(X%*%fit_d$beta)
+delta_g = fit_d$delta_g
+delta_r = fit_d$delta_r
+lam_d = matchy(fit_d$tt, fit_d$lam, tt)
+lam_od1 = sapply(1:l, function(t) lam_d[t]*exp(Xb))
+lam_ogd1 = lam_od1 * exp(delta_g)
+lam_ord1 = lam_od1 * exp(delta_r)
+lam_ogrd1 = lam_orgd1 = lam_od1 * exp(delta_g+delta_r)
+fit_d = try(phfit_d(Tg,Dg,Tr,Dr,Td,Dd,A,X,a=0))
+if ('try-error' %in% fit_d) next
+Xb = as.numeric(X%*%fit_d$beta)
+delta_g = fit_d$delta_g
+delta_r = fit_d$delta_r
+lam_d = matchy(fit_d$tt, fit_d$lam, tt)
+lam_od0 = sapply(1:l, function(t) lam_d[t]*exp(Xb))
+lam_ogd0 = lam_od0 * exp(delta_g)
+lam_ord0 = lam_od0 * exp(delta_r)
+lam_ogrd0 = lam_orgd0 = lam_od0 * exp(delta_g+delta_r)
+# hazard of g
+fit_g = try(phfit_g(Tg,Dg,Tr,Dr,Td,Dd,A,X,a=1))
+if ('try-error' %in% fit_g) next
+Xb = as.numeric(X%*%fit_g$beta)
+delta_r = fit_g$delta_r
+lam_g = matchy(fit_g$tt, fit_g$lam, tt)
+lam_og1 = sapply(1:l, function(t) lam_g[t]*exp(Xb))
+lam_org1 = lam_og1 *exp(delta_r)
+fit_g = try(phfit_g(Tg,Dg,Tr,Dr,Td,Dd,A,X,a=0))
+if ('try-error' %in% fit_g) next
+Xb = as.numeric(X%*%fit_g$beta)
+delta_r = fit_g$delta_r
+lam_g = matchy(fit_g$tt, fit_g$lam, tt)
+lam_og0 = sapply(1:l, function(t) lam_g[t]*exp(Xb))
+lam_org0 = lam_og0 *exp(delta_r)
+# hazard of r
+fit_r = try(phfit_r(Tg,Dg,Tr,Dr,Td,Dd,A,X,a=1))
+if ('try-error' %in% fit_r) next
+Xb = as.numeric(X%*%fit_r$beta)
+delta_g = fit_r$delta_g
+lam_r = matchy(fit_r$tt, fit_r$lam, tt)
+lam_or1 = sapply(1:l, function(t) lam_r[t]*exp(Xb))
+lam_ogr1 = lam_or1 * exp(delta_g)
+fit_r = try(phfit_r(Tg,Dg,Tr,Dr,Td,Dd,A,X,a=0))
+if ('try-error' %in% fit_r) next
+Xb = as.numeric(X%*%fit_r$beta)
+delta_g = fit_r$delta_g
+lam_r = matchy(fit_r$tt, fit_r$lam, tt)
+lam_or0 = sapply(1:l, function(t) lam_r[t]*exp(Xb))
+lam_ogr0 = lam_or0 * exp(delta_g)
+
+fit = glm(A~X, family='binomial')
+ps = matrix(1,nrow=n,ncol=2)
+pscore = predict(fit, type='response')
+ps[,1] = (1 - pscore) #* mean((1-A)/(1-pscore))
+ps[,2] = pscore #* mean(A/pscore)
+
+# potential incidence
+Lam_o_1 = t(apply(lam_od1+lam_og1+lam_or1, 1, cumsum))
+Lam_og_1 = t(apply(lam_ogd1+lam_ogr1, 1, cumsum))
+Lam_or_1 = t(apply(lam_ord1+lam_org1, 1, cumsum))
+Lam_ogr_1 = t(apply(lam_ogrd1, 1, cumsum))
+Lam_org_1 = t(apply(lam_orgd1, 1, cumsum))
+dF_or_1 = exp(-Lam_o_1)*lam_or1
+dF_og_1 = exp(-Lam_o_1)*lam_og1
+dF_od_1 = exp(-Lam_o_1)*lam_od1
+F_or_1 = t(apply(dF_or_1, 1, cumsum))
+F_og_1 = t(apply(dF_og_1, 1, cumsum))
+F_od_1 = t(apply(dF_od_1, 1, cumsum))
+dF_og__1 = t(apply(dF_og_1*exp(Lam_og_1), 1, cumsum))*exp(-Lam_og_1)
+dF_or__1 = t(apply(dF_or_1*exp(Lam_or_1), 1, cumsum))*exp(-Lam_or_1)
+dF_ogr_1 = dF_og__1*lam_ogr1
+F_ogr_1 = t(apply(dF_ogr_1, 1, cumsum))
+dF_org_1 = dF_or__1*lam_org1
+F_org_1 = t(apply(dF_org_1, 1, cumsum))
+dF_ord_1 = dF_or__1*lam_ord1
+F_ord_1 = t(apply(dF_ord_1, 1, cumsum))
+dF_ogd_1 = dF_og__1*lam_ogd1
+F_ogd_1 = t(apply(dF_ogd_1, 1, cumsum))
+dF_org__1 = t(apply(dF_org_1*exp(Lam_org_1), 1, cumsum))*exp(-Lam_org_1)
+dF_ogr__1 = t(apply(dF_ogr_1*exp(Lam_ogr_1), 1, cumsum))*exp(-Lam_ogr_1)
+dF_orgd_1 = dF_org__1*lam_orgd1
+F_orgd_1 = t(apply(dF_orgd_1, 1, cumsum))
+dF_ogrd_1 = dF_ogr__1*lam_ogrd1
+F_ogrd_1 = t(apply(dF_ogrd_1, 1, cumsum))
+F_1.b = colMeans(F_od_1+F_ord_1+F_ogd_1+F_orgd_1+F_ogrd_1)
+
+Lam_o_0 = t(apply(lam_od0+lam_og0+lam_or0, 1, cumsum))
+Lam_og_0 = t(apply(lam_ogd0+lam_ogr0, 1, cumsum))
+Lam_or_0 = t(apply(lam_ord0+lam_org0, 1, cumsum))
+Lam_ogr_0 = t(apply(lam_ogrd0, 1, cumsum))
+Lam_org_0 = t(apply(lam_orgd0, 1, cumsum))
+dF_or_0 = exp(-Lam_o_0)*lam_or0
+dF_og_0 = exp(-Lam_o_0)*lam_og0
+dF_od_0 = exp(-Lam_o_0)*lam_od0
+F_or_0 = t(apply(dF_or_0, 1, cumsum))
+F_og_0 = t(apply(dF_og_0, 1, cumsum))
+F_od_0 = t(apply(dF_od_0, 1, cumsum))
+dF_og__0 = t(apply(dF_og_0*exp(Lam_og_0), 1, cumsum))*exp(-Lam_og_0)
+dF_or__0 = t(apply(dF_or_0*exp(Lam_or_0), 1, cumsum))*exp(-Lam_or_0)
+dF_ogr_0 = dF_og__0*lam_ogr0
+F_ogr_0 = t(apply(dF_ogr_0, 1, cumsum))
+dF_org_0 = dF_or__0*lam_org0
+F_org_0 = t(apply(dF_org_0, 1, cumsum))
+dF_ord_0 = dF_or__0*lam_ord0
+F_ord_0 = t(apply(dF_ord_0, 1, cumsum))
+dF_ogd_0 = dF_og__0*lam_ogd0
+F_ogd_0 = t(apply(dF_ogd_0, 1, cumsum))
+dF_org__0 = t(apply(dF_org_0*exp(Lam_org_0), 1, cumsum))*exp(-Lam_org_0)
+dF_ogr__0 = t(apply(dF_ogr_0*exp(Lam_ogr_0), 1, cumsum))*exp(-Lam_ogr_0)
+dF_orgd_0 = dF_org__0*lam_orgd0
+F_orgd_0 = t(apply(dF_orgd_0, 1, cumsum))
+dF_ogrd_0 = dF_ogr__0*lam_ogrd0
+F_ogrd_0 = t(apply(dF_ogrd_0, 1, cumsum))
+F_0.b = colMeans(F_od_0+F_ord_0+F_ogd_0+F_orgd_0+F_ogrd_0)
+
+# counterfactual incidence, separable
+# a_or=a_gr=a_og=a_rg=0,a_od=a_rd=a_gd=1
+lam_og_a = lam_og0
+lam_od_a = lam_od1
+lam_or_a = lam_or0
+lam_ogr_a = lam_ogr0
+lam_ogd_a = lam_ogd1
+lam_org_a = lam_org0
+lam_ord_a = lam_ord1
+lam_ogrd_a = lam_ogrd1
+lam_orgd_a = lam_orgd1
+Lam_o_a = t(apply(lam_od_a+lam_og_a+lam_or_a, 1, cumsum))
+Lam_og_a = t(apply(lam_ogd_a+lam_ogr_a, 1, cumsum))
+Lam_or_a = t(apply(lam_ord_a+lam_org_a, 1, cumsum))
+Lam_ogr_a = t(apply(lam_ogrd_a, 1, cumsum))
+Lam_org_a = t(apply(lam_orgd_a, 1, cumsum))
+dF_or_a = exp(-Lam_o_a)*lam_or_a
+dF_og_a = exp(-Lam_o_a)*lam_og_a
+dF_od_a = exp(-Lam_o_a)*lam_od_a
+F_or_a = t(apply(dF_or_a, 1, cumsum))
+F_og_a = t(apply(dF_og_a, 1, cumsum))
+F_od_a = t(apply(dF_od_a, 1, cumsum))
+dF_og__a = t(apply(dF_og_a*exp(Lam_og_a), 1, cumsum))*exp(-Lam_og_a)
+dF_or__a = t(apply(dF_or_a*exp(Lam_or_a), 1, cumsum))*exp(-Lam_or_a)
+dF_ogr_a = dF_og__a*lam_ogr_a
+F_ogr_a = t(apply(dF_ogr_a, 1, cumsum))
+dF_org_a = dF_or__a*lam_org_a
+F_org_a = t(apply(dF_org_a, 1, cumsum))
+dF_ord_a = dF_or__a*lam_ord_a
+F_ord_a = t(apply(dF_ord_a, 1, cumsum))
+dF_ogd_a = dF_og__a*lam_ogd_a
+F_ogd_a = t(apply(dF_ogd_a, 1, cumsum))
+dF_org__a = t(apply(dF_org_a*exp(Lam_org_a), 1, cumsum))*exp(-Lam_org_a)
+dF_ogr__a = t(apply(dF_ogr_a*exp(Lam_ogr_a), 1, cumsum))*exp(-Lam_ogr_a)
+dF_orgd_a = dF_org__a*lam_orgd_a
+F_orgd_a = t(apply(dF_orgd_a, 1, cumsum))
+dF_ogrd_a = dF_ogr__a*lam_ogrd_a
+F_ogrd_a = t(apply(dF_ogrd_a, 1, cumsum))
+Fd_spe.b = colMeans(F_od_a + F_ogd_a + F_ord_a + F_ogrd_a + F_orgd_a)
+
+# counterfactual incidence, conditionally interventional
+# a_or=a_gr=0,a_od=a_og=a_rd=a_gd=a_rg=1
+lam_og_a = lam_og1
+lam_od_a = lam_od1
+lam_or_a = lam_or0
+lam_ogr_a = lam_ogr0
+lam_ogd_a = lam_ogd1
+lam_org_a = lam_org1
+lam_ord_a = lam_ord1
+lam_ogrd_a = lam_ogrd1
+lam_orgd_a = lam_orgd1
+Lam_o_a = t(apply(lam_od_a+lam_og_a+lam_or_a, 1, cumsum))
+Lam_og_a = t(apply(lam_ogd_a+lam_ogr_a, 1, cumsum))
+Lam_or_a = t(apply(lam_ord_a+lam_org_a, 1, cumsum))
+Lam_ogr_a = t(apply(lam_ogrd_a, 1, cumsum))
+Lam_org_a = t(apply(lam_orgd_a, 1, cumsum))
+dF_or_a = exp(-Lam_o_a)*lam_or_a
+dF_og_a = exp(-Lam_o_a)*lam_og_a
+dF_od_a = exp(-Lam_o_a)*lam_od_a
+F_or_a = t(apply(dF_or_a, 1, cumsum))
+F_og_a = t(apply(dF_og_a, 1, cumsum))
+F_od_a = t(apply(dF_od_a, 1, cumsum))
+dF_og__a = t(apply(dF_og_a*exp(Lam_og_a), 1, cumsum))*exp(-Lam_og_a)
+dF_or__a = t(apply(dF_or_a*exp(Lam_or_a), 1, cumsum))*exp(-Lam_or_a)
+dF_ogr_a = dF_og__a*lam_ogr_a
+F_ogr_a = t(apply(dF_ogr_a, 1, cumsum))
+dF_org_a = dF_or__a*lam_org_a
+F_org_a = t(apply(dF_org_a, 1, cumsum))
+dF_ord_a = dF_or__a*lam_ord_a
+F_ord_a = t(apply(dF_ord_a, 1, cumsum))
+dF_ogd_a = dF_og__a*lam_ogd_a
+F_ogd_a = t(apply(dF_ogd_a, 1, cumsum))
+dF_org__a = t(apply(dF_org_a*exp(Lam_org_a), 1, cumsum))*exp(-Lam_org_a)
+dF_ogr__a = t(apply(dF_ogr_a*exp(Lam_ogr_a), 1, cumsum))*exp(-Lam_ogr_a)
+dF_orgd_a = dF_org__a*lam_orgd_a
+F_orgd_a = t(apply(dF_orgd_a, 1, cumsum))
+dF_ogrd_a = dF_ogr__a*lam_ogrd_a
+F_ogrd_a = t(apply(dF_ogrd_a, 1, cumsum))
+Fd_cin.b = colMeans(F_od_a + F_ogd_a + F_ord_a + F_ogrd_a + F_orgd_a)
+
+# potential incidence, overall
+# lam_or = lam_ogr
+lam_og_a = lam_og1
+lam_od_a = lam_od1
+lam_ogd_a = lam_ogd1
+lam_org_a = lam_org1
+lam_ord_a = lam_ord1
+lam_ogrd_a = lam_ogrd1
+lam_orgd_a = lam_orgd1
+prev_g = (F_og_1-F_ogr_1-F_ogd_1)/(1-F_od_1-F_or_1-F_ogd_1-F_ogr_1)
+lam_or_a = lam_ogr_a = prev_g*lam_ogr1+(1-prev_g)*lam_or1
+Lam_o_a = t(apply(lam_od_a+lam_og_a+lam_or_a, 1, cumsum))
+Lam_og_a = t(apply(lam_ogd_a+lam_ogr_a, 1, cumsum))
+Lam_or_a = t(apply(lam_ord_a+lam_org_a, 1, cumsum))
+Lam_ogr_a = t(apply(lam_ogrd_a, 1, cumsum))
+Lam_org_a = t(apply(lam_orgd_a, 1, cumsum))
+dF_or_a = exp(-Lam_o_a)*lam_or_a
+dF_og_a = exp(-Lam_o_a)*lam_og_a
+dF_od_a = exp(-Lam_o_a)*lam_od_a
+F_or_a = t(apply(dF_or_a, 1, cumsum))
+F_og_a = t(apply(dF_og_a, 1, cumsum))
+F_od_a = t(apply(dF_od_a, 1, cumsum))
+dF_og__a = t(apply(dF_og_a*exp(Lam_og_a), 1, cumsum))*exp(-Lam_og_a)
+dF_or__a = t(apply(dF_or_a*exp(Lam_or_a), 1, cumsum))*exp(-Lam_or_a)
+dF_ogr_a = dF_og__a*lam_ogr_a
+F_ogr_a = t(apply(dF_ogr_a, 1, cumsum))
+dF_org_a = dF_or__a*lam_org_a
+F_org_a = t(apply(dF_org_a, 1, cumsum))
+dF_ord_a = dF_or__a*lam_ord_a
+F_ord_a = t(apply(dF_ord_a, 1, cumsum))
+dF_ogd_a = dF_og__a*lam_ogd_a
+F_ogd_a = t(apply(dF_ogd_a, 1, cumsum))
+dF_org__a = t(apply(dF_org_a*exp(Lam_org_a), 1, cumsum))*exp(-Lam_org_a)
+dF_ogr__a = t(apply(dF_ogr_a*exp(Lam_ogr_a), 1, cumsum))*exp(-Lam_ogr_a)
+dF_orgd_a = dF_org__a*lam_orgd_a
+F_orgd_a = t(apply(dF_orgd_a, 1, cumsum))
+dF_ogrd_a = dF_ogr__a*lam_ogrd_a
+F_ogrd_a = t(apply(dF_ogrd_a, 1, cumsum))
+F_1o.b = colMeans(F_od_a + F_ogd_a + F_ord_a + F_ogrd_a + F_orgd_a)
+
+# lam_or = lam_ogr
+lam_og_a = lam_og0
+lam_od_a = lam_od0
+lam_ogd_a = lam_ogd0
+lam_org_a = lam_org0
+lam_ord_a = lam_ord0
+lam_ogrd_a = lam_ogrd0
+lam_orgd_a = lam_orgd0
+prev_g = (F_og_0-F_ogr_0-F_ogd_0)/(1-F_od_0-F_or_0-F_ogd_0-F_ogr_0)
+lam_or_a = lam_ogr_a = prev_g*lam_ogr0+(1-prev_g)*lam_or0
+Lam_o_a = t(apply(lam_od_a+lam_og_a+lam_or_a, 1, cumsum))
+Lam_og_a = t(apply(lam_ogd_a+lam_ogr_a, 1, cumsum))
+Lam_or_a = t(apply(lam_ord_a+lam_org_a, 1, cumsum))
+Lam_ogr_a = t(apply(lam_ogrd_a, 1, cumsum))
+Lam_org_a = t(apply(lam_orgd_a, 1, cumsum))
+dF_or_a = exp(-Lam_o_a)*lam_or_a
+dF_og_a = exp(-Lam_o_a)*lam_og_a
+dF_od_a = exp(-Lam_o_a)*lam_od_a
+F_or_a = t(apply(dF_or_a, 1, cumsum))
+F_og_a = t(apply(dF_og_a, 1, cumsum))
+F_od_a = t(apply(dF_od_a, 1, cumsum))
+dF_og__a = t(apply(dF_og_a*exp(Lam_og_a), 1, cumsum))*exp(-Lam_og_a)
+dF_or__a = t(apply(dF_or_a*exp(Lam_or_a), 1, cumsum))*exp(-Lam_or_a)
+dF_ogr_a = dF_og__a*lam_ogr_a
+F_ogr_a = t(apply(dF_ogr_a, 1, cumsum))
+dF_org_a = dF_or__a*lam_org_a
+F_org_a = t(apply(dF_org_a, 1, cumsum))
+dF_ord_a = dF_or__a*lam_ord_a
+F_ord_a = t(apply(dF_ord_a, 1, cumsum))
+dF_ogd_a = dF_og__a*lam_ogd_a
+F_ogd_a = t(apply(dF_ogd_a, 1, cumsum))
+dF_org__a = t(apply(dF_org_a*exp(Lam_org_a), 1, cumsum))*exp(-Lam_org_a)
+dF_ogr__a = t(apply(dF_ogr_a*exp(Lam_ogr_a), 1, cumsum))*exp(-Lam_ogr_a)
+dF_orgd_a = dF_org__a*lam_orgd_a
+F_orgd_a = t(apply(dF_orgd_a, 1, cumsum))
+dF_ogrd_a = dF_ogr__a*lam_ogrd_a
+F_ogrd_a = t(apply(dF_ogrd_a, 1, cumsum))
+F_0o.b = colMeans(F_od_a + F_ogd_a + F_ord_a + F_ogrd_a + F_orgd_a)
+
+# counterfactual incidence, interventional
+# lam_or = lam_ogr
+lam_og_a = lam_og1
+lam_od_a = lam_od1
+lam_ogd_a = lam_ogd1
+lam_org_a = lam_org1
+lam_ord_a = lam_ord1
+lam_ogrd_a = lam_ogrd1
+lam_orgd_a = lam_orgd1
+prev_g = (F_og_0-F_ogr_0-F_ogd_0)/(1-F_od_0-F_or_0-F_ogd_0-F_ogr_0)
+lam_or_a = lam_ogr_a = prev_g*lam_ogr0+(1-prev_g)*lam_or0
+Lam_o_a = t(apply(lam_od_a+lam_og_a+lam_or_a, 1, cumsum))
+Lam_og_a = t(apply(lam_ogd_a+lam_ogr_a, 1, cumsum))
+Lam_or_a = t(apply(lam_ord_a+lam_org_a, 1, cumsum))
+Lam_ogr_a = t(apply(lam_ogrd_a, 1, cumsum))
+Lam_org_a = t(apply(lam_orgd_a, 1, cumsum))
+dF_or_a = exp(-Lam_o_a)*lam_or_a
+dF_og_a = exp(-Lam_o_a)*lam_og_a
+dF_od_a = exp(-Lam_o_a)*lam_od_a
+F_or_a = t(apply(dF_or_a, 1, cumsum))
+F_og_a = t(apply(dF_og_a, 1, cumsum))
+F_od_a = t(apply(dF_od_a, 1, cumsum))
+dF_og__a = t(apply(dF_og_a*exp(Lam_og_a), 1, cumsum))*exp(-Lam_og_a)
+dF_or__a = t(apply(dF_or_a*exp(Lam_or_a), 1, cumsum))*exp(-Lam_or_a)
+dF_ogr_a = dF_og__a*lam_ogr_a
+F_ogr_a = t(apply(dF_ogr_a, 1, cumsum))
+dF_org_a = dF_or__a*lam_org_a
+F_org_a = t(apply(dF_org_a, 1, cumsum))
+dF_ord_a = dF_or__a*lam_ord_a
+F_ord_a = t(apply(dF_ord_a, 1, cumsum))
+dF_ogd_a = dF_og__a*lam_ogd_a
+F_ogd_a = t(apply(dF_ogd_a, 1, cumsum))
+dF_org__a = t(apply(dF_org_a*exp(Lam_org_a), 1, cumsum))*exp(-Lam_org_a)
+dF_ogr__a = t(apply(dF_ogr_a*exp(Lam_ogr_a), 1, cumsum))*exp(-Lam_ogr_a)
+dF_orgd_a = dF_org__a*lam_orgd_a
+F_orgd_a = t(apply(dF_orgd_a, 1, cumsum))
+dF_ogrd_a = dF_ogr__a*lam_ogrd_a
+F_ogrd_a = t(apply(dF_ogrd_a, 1, cumsum))
+Fd_int.b = colMeans(F_od_a + F_ogd_a + F_ord_a + F_ogrd_a + F_orgd_a)
+
+cif1 = rbind(cif1, F_1.b)
+cif0 = rbind(cif0, F_0.b)
+cif1o = rbind(cif1o, F_1o.b)
+cif0o = rbind(cif0o, F_0o.b)
+cif_spe = rbind(cif_spe, Fd_spe.b)
+cif_cin = rbind(cif_cin, Fd_cin.b)
+cif_int = rbind(cif_int, Fd_int.b)
+}
--- phfit.R
+++ phfit.R
@@ -1,6 +1,6 @@
 library(MASS)
 
-phfit_d = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
+phfit_d = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a,par=NULL){
   Tg = Tg[A==a]
   Tr = Tr[A==a]
   Td = Td[A==a]
@@ -11,10 +11,19 @@ phfit_d = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
   tt = sort(unique(Td[Dd==1]))
   l = length(tt)
   k = length(Td)
-  lam = rep(1/l, l)
   p = ncol(X)
-  beta = beta0 = rep(0, p)
-  delta_g = delta_r = delta_g0 = delta_r0 = 0
+  if (!is.null(par)){
+    beta = beta0 = par[2+1:p]
+    delta_g = delta_g0 = par[1]
+    delta_r = delta_r0 = par[2]
+  } else {
+    beta = beta0 = rep(0, p)
+    delta_g = delta_r = delta_g0 = delta_r0 = 0
+  }
+  Xb = as.numeric(X%*%beta)
+  lam = sapply(tt, function(t) sum(Dd*(Td==t))/sum((Td>=t)*
+                       exp(Xb+(Tg<t)*delta_g+(Tr<t)*delta_r)))
+  lam[is.nan(lam)] = 0
   while(TRUE){
     Xb = as.numeric(X%*%beta)
     Lam = sapply(1:k, function(i) sum((tt<=Td[i])*lam*
@@ -22,12 +31,25 @@ phfit_d = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
     Lam = Lam * exp(Xb)
     dbeta = t(X)%*%(Dd-Lam)
     ddbeta = -t(X)%*%diag(Lam)%*%X
+    #S0 = sapply(Td, function(l) sum((Td>=l)*exp(Xb+delta_g*Dg*(Tg<l)+delta_r*Dr*(Tr<l))))
+    #S1 = sapply(Td, function(l) colSums((Td>=l)*exp(Xb+delta_g*Dg*(Tg<l)+delta_r*Dr*(Tr<l))*X))
+    #S2 = sapply(Td, function(l) t(X)%*%diag((Td>=l)*exp(Xb+delta_g*Dg*(Tg<l)+delta_r*Dr*(Tr<l)))%*%X)
+    #S1 = t(S1)
+    #S2 = t(S2)
+    #dbeta = as.numeric(t(X-S1/S0)%*%Dd)
+    #ddbeta = t(S1/S0)%*%diag(Dd)%*%(S1/S0) - matrix(colSums(Dd*S2/S0),p,p)
     dddelta_g = - sum(sapply(1:k, function(i) sum((tt<=Td[i])*lam*
                          (tt>Tg[i])*exp(Xb[i]+delta_g+(tt>Tr[i])*delta_r))))
     dddelta_r = - sum(sapply(1:k, function(i) sum((tt<=Td[i])*lam*
                          (tt>Tr[i])*exp(Xb[i]+delta_r+(tt>Tg[i])*delta_g))))
     ddelta_g = dddelta_g + sum(Dd*(Tg<Td))
     ddelta_r = dddelta_r + sum(Dd*(Tr<Td))
+    #S1 = sapply(Td, function(l) sum((Td>=l)*exp(Xb+delta_g*Dg*(Tg<l)+delta_r*Dr*(Tr<l))*Dg*(Tg<l)))
+    #ddelta_g = sum(Dd*(Dg-S1/S0))
+    #dddelta_g = sum(Dd*(S1/S0)^2) - sum(Dd*S1/S0^2)
+    #S1 = sapply(Td, function(l) sum((Td>=l)*exp(Xb+delta_g*Dg*(Tg<l)+delta_r*Dr*(Tr<l))*Dr*(Tr<l)))
+    #ddelta_r = sum(Dd*(Dr-S1/S0))
+    #dddelta_r = sum(Dd*(S1/S0)^2) - sum(Dd*S1/S0^2)
     beta = beta - ginv(ddbeta) %*% dbeta
     if (sum(Dd*(Tg<Td))==0) {
       delta_g = 0
@@ -44,14 +66,14 @@ phfit_d = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
                                 exp(Xb+(Tg<t)*delta_g+(Tr<t)*delta_r)))
     lam[is.nan(lam)] = 0
     tol = max(abs(c(beta-beta0,delta_g-delta_g0,delta_r-delta_r0)))
-    print(c(delta_g,delta_r,beta,tol))
-    if (tol<0.0001) break
+    #print(c(delta_g,delta_r,beta,tol))
+    if (tol<0.00001) break
     beta0 = beta; delta_g0 = delta_g; delta_r0 = delta_r
   }
   return(list(beta=beta,delta_g=delta_g,delta_r=delta_r,tt=tt,lam=lam))
 }
 
-phfit_g = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
+phfit_g = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a,par=NULL){
   Tg = Tg[A==a]
   Tr = Tr[A==a]
   Td = Td[A==a]
@@ -62,10 +84,18 @@ phfit_g = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
   tt = sort(unique(Tg[Dg==1]))
   l = length(tt)
   k = length(Tg)
-  lam = rep(1/l, l)
   p = ncol(X)
-  beta = beta0 = rep(0, p)
-  delta_r = delta_r0 = 0
+  if (!is.null(par)){
+    beta = beta0 = par[1+1:p]
+    delta_r = delta_r0 = par[1]
+  } else {
+    beta = beta0 = rep(0, p)
+    delta_r = delta_r0 = 0
+  }
+  Xb = as.numeric(X%*%beta)
+  lam = sapply(tt, function(t) sum(Dg*(Tg==t))/sum((Tg>=t)*
+                       exp(Xb+(Tr<t)*delta_r)))
+  lam[is.nan(lam)] = 0
   while(TRUE){
     Xb = as.numeric(X%*%beta)
     Lam = sapply(1:k, function(i) sum((tt<=Tg[i])*lam*exp((tt>Tr[i])*delta_r)))
@@ -86,14 +116,14 @@ phfit_g = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
                                exp(Xb+(Tr<t)*delta_r)))
     lam[is.nan(lam)] = 0
     tol = max(abs(c(beta-beta0,delta_r-delta_r0)))
-    print(c(delta_r,beta,tol))
-    if (tol<0.0001) break
+    #print(c(delta_r,beta,tol))
+    if (tol<0.00001) break
     beta0 = beta; delta_r0 = delta_r
   }
   return(list(beta=beta,delta_r=delta_r,tt=tt,lam=lam))
 }
 
-phfit_r = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
+phfit_r = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a,par=NULL){
   Tg = Tg[A==a]
   Tr = Tr[A==a]
   Td = Td[A==a]
@@ -104,10 +134,18 @@ phfit_r = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
   tt = sort(unique(Tr[Dr==1]))
   l = length(tt)
   k = length(Tr)
-  lam = rep(1/l, l)
   p = ncol(X)
-  beta = beta0 = rep(0, p)
-  delta_g = delta_g0 = 0
+  if (!is.null(par)){
+    beta = beta0 = par[1+1:p]
+    delta_g = delta_g0 = par[1]
+  } else {
+    beta = beta0 = rep(0, p)
+    delta_g = delta_g0 = 0
+  }
+  Xb = as.numeric(X%*%beta)
+  lam = sapply(tt, function(t) sum(Dr*(Tr==t))/sum((Tr>=t)*
+                               exp(Xb+(Tg<t)*delta_g)))
+  lam[is.nan(lam)] = 0
   while(TRUE){
     Xb = as.numeric(X%*%beta)
     Lam = sapply(1:k, function(i) sum((tt<=Tr[i])*lam*exp((tt>Tg[i])*delta_g)))
@@ -128,33 +166,34 @@ phfit_r = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
                                                        exp(Xb+(Tg<t)*delta_g)))
     lam[is.nan(lam)] = 0
     tol = max(abs(c(beta-beta0,delta_g-delta_g0)))
-    print(c(delta_g,beta,tol))
-    if (tol<0.0001) break
+    #print(c(delta_g,beta,tol))
+    if (tol<0.00001) break
     beta0 = beta; delta_g0 = delta_g
   }
   return(list(beta=beta,delta_g=delta_g,tt=tt,lam=lam))
 }
 
-phfit_c = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
-  Tg = Tg[A==a]
-  Tr = Tr[A==a]
+phfit_c = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a,par=NULL){
   Td = Td[A==a]
-  Dg = Dg[A==a]
-  Dr = Dr[A==a]
   Dd = Dd[A==a]
   X = X[A==a,]
   Tc = Td
   Dc = 1 - Dd
   tt = sort(unique(Td[Dd==0]))
   l = length(tt)
-  k = length(Tg)
-  lam = rep(1/l, l)
+  k = length(Tc)
   if (!is.null(X)) {
     p = ncol(X)
     beta = beta0 = rep(0, p)
   } else {
     beta = beta0 = 0
   }
+  if (!is.null(par)){
+    beta = beta0 = par
+  }
+  Xb = as.numeric(X%*%beta)
+  lam = sapply(tt, function(t) sum(Dc*(Tc==t))/sum((Tc>=t)*exp(Xb)))
+  lam[is.nan(lam)] = 0
   while(TRUE){
     if (!is.null(X)) {
       Xb = as.numeric(X%*%beta)
@@ -171,39 +210,41 @@ phfit_c = function(Tg,Dg,Tr,Dr,Td,Dd,A,X,a){
     lam = sapply(tt, function(t) sum(Dc*(Tc==t))/sum((Tc>=t)*exp(Xb)))
     lam[is.nan(lam)] = 0
     tol = max(abs(c(beta-beta0)))
-    print(c(beta,tol))
-    if (tol<0.0001) break
+    #print(c(beta,tol))
+    if (tol<0.00001) break
     beta0 = beta
   }
   return(list(beta=beta,tt=tt,lam=lam))
 }
 
-phfit_3 = function(Tr,Dr,Td,Dd,A,X,a){
-  Tr = Tr[A==a]
-  Td = Td[A==a]
-  Dr = Dr[A==a]
-  Dd = Dd[A==a]
-  X = X[A==a,]
-  tt = sort(unique(Td[Dd==1]))
-  l = length(tt)
-  k = length(Td)
-  lam = rep(1/l, l)
-  p = ncol(X)
-  beta = beta0 = rep(0, p)
-  while(TRUE){
-    Xb = as.numeric(X%*%beta)
-    Lam = sapply(1:k, function(i) sum((tt<=Td[i])*(tt>=Tr[i])*(Dr[i]==1)*lam))
-    Lam = Lam * exp(Xb)
-    dbeta = t(X)%*%(Dd*Dr-Lam)
-    ddbeta = -t(X)%*%diag(Lam)%*%X
-    beta = beta - ginv(ddbeta) %*% dbeta
-    Xb = as.numeric(X%*%beta)
-    lam = sapply(tt, function(t) sum(Dd*(Td==t)*Dr)/sum((Td>=t)*Dr*(Tr<=t)*
-                                     exp(Xb)))
-    lam[is.nan(lam)] = 0
-    tol = max(abs(beta-beta0))
-    if (tol<0.0001) break
-    beta0 = beta
+
+testcif <- function(fit1,fit0,maxt=NULL){
+  tt = fit1$tt
+  F1 = colMeans(fit1$Feff, na.rm=TRUE)
+  F0 = colMeans(fit0$Feff, na.rm=TRUE)
+  IF1 = fit1$EIF
+  IF0 = fit0$EIF
+  Ti = rep(TRUE, length(tt))
+  if (!is.null(maxt)) Ti = (tt<=maxt)
+  Tt = sum((F1*(1-F1)-F0*(1-F0))*diff(c(0,tt))*Ti)
+  IFt = colSums(((1-2*F1)*t(IF1)-(1-2*F0)*t(IF0))*diff(c(0,tt))*Ti)
+  Vt = sd(IFt, na.rm=TRUE)/sqrt(nrow(IF1))
+  p1 = 2*pnorm(-abs(Tt/Vt))
+  Tt = sum((F1-F0)*diff(c(0,F1+F0))*Ti)
+  V1 = colSums(t(IF1-IF0)*diff(c(0,F1+F0))*Ti)
+  V2 = colSums((F1-F0)*apply(cbind(0,IF1+IF0),1,diff)*Ti)
+  Vt = sd(V1+V2)/sqrt(nrow(IF1))
+  p2 = 2*pnorm(-abs(Tt/Vt))
+  return(list(p1=p1,p2=p2))
+}
+
+matchy = function(x,y,newx,exact=TRUE){
+  if (exact) {
+    newy = sapply(newx, function(t) y[max(which(x==t))])
+  } else {
+    newy = sapply(newx, function(t) y[max(which(x<=t))])
   }
-  return(list(beta=beta,tt=tt,lam=lam))
+  newy = as.numeric(newy)
+  newy[is.na(newy)] = 0
+  return(newy)
 }
